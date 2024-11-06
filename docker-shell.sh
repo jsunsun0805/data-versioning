3487e7f84994d3a4f2f53fec0642dd8a7a2e3c63
Jiayi Sun <jsunsun0805@gmail.com>
2024-11-05 21:34:25 -0500

    dataset updates...

--- docker-shell.sh
+++ docker-shell.sh
@@ -4,8 +4,8 @@ set -e
 
 export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/../secrets/
-export GCS_BUCKET_NAME="cheese-app-data-versioning"
-export GCP_PROJECT="ac215-project"
+export GCS_BUCKET_NAME="cheese-app-data-versioning-jsunsun"
+export GCP_PROJECT="AC215"
 export GCP_ZONE="us-central1-a"
 export GOOGLE_APPLICATION_CREDENTIALS="/secrets/data-service-account.json"
 
